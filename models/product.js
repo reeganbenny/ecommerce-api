1ee8eb0a625bda4dcb9201ef7a88ead2ad84cded
reeganbenny <reeganbenny@gmail.com>
2023-07-19 12:33:58 +0530

    product Schema

---
+++ models/product.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    variants: [
+      {
+        variantsId: {
+          type: Schema.Types.ObjectId,
+          ref: "variants",
+          required: true,
+        },
+        SKU: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("product", productSchema);
