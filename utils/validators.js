dd4981168111ab3e9f4aef68efae34e2b3cb8601
reeganbenny <reeganbenny@gmail.com>
2023-07-19 22:42:12 +0530

    add validation for variants

--- utils/validators.js
+++ utils/validators.js
@@ -32,3 +32,43 @@ exports.updateProductValidator = [
     .optional({ nullable: true })
     .withMessage("price is required"),
 ];
+
+exports.createVariantValidator = [
+  check("name").trim().notEmpty().withMessage("Name is required"),
+  check("SKU").trim().notEmpty().withMessage("Description is required"),
+  check("additionalCost")
+    .trim()
+    .isNumeric()
+    .notEmpty()
+    .withMessage("price is required"),
+  check("stockCount")
+    .trim()
+    .isNumeric()
+    .notEmpty()
+    .withMessage("price is required"),
+];
+
+exports.updateVariantValidator = [
+  check("name")
+    .trim()
+    .notEmpty()
+    .optional({ nullable: true })
+    .withMessage("Name is required"),
+  check("SKU")
+    .trim()
+    .notEmpty()
+    .optional({ nullable: true })
+    .withMessage("Description is required"),
+  check("additionalCost")
+    .trim()
+    .isNumeric()
+    .notEmpty()
+    .optional({ nullable: true })
+    .withMessage("price is required"),
+  check("stockCount")
+    .trim()
+    .isNumeric()
+    .notEmpty()
+    .optional({ nullable: true })
+    .withMessage("price is required"),
+];
